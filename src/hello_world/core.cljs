50d3c0c79763386bf7cb96c82524cfe97094beab
Alexei Matveev <alexei.matveev@gmail.com>
2016-09-17 00:58:17 +0200

    AM: combine components

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -9,17 +9,36 @@ ahead and edit it and see reloading in action.")
 
 (js/console.log "Log message")
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:text "Hello world!"}))
+;; Define your app data so that it doesn't get over-written on reload
+(defonce app-state (r/atom {:text "Hello world!"}))
 
 (defn some-component []
   [:div
-   [:h3 "I am a component!"]
+   [:h3 "I am component 1!"]
    [:p.someclass
     "I have " [:strong "bold"]
     [:span {:style {:color "blue"}} " and blue"]
     " text."]])
+
+(defn timer-component []
+  (let [seconds-elapsed (r/atom 0)]
+    (fn []
+      (js/setTimeout #(swap! seconds-elapsed inc) 1000)
+      [:div
+       [:h3 "I am component 2!"]
+       [:p.someclass  "Seconds Elapsed: " @seconds-elapsed]])))
+
+(defn stateful-component []
+  [:div
+   [:h3 "I am a stateful component!"]
+   [:p.someclass (:text @app-state)]])
+
+(defn app []
+  [:div
+   [:h2 "I am supercomponent!"]
+   [some-component]
+   [timer-component]
+   [stateful-component]])
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -27,5 +46,5 @@ ahead and edit it and see reloading in action.")
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
   )
 
-(r/render-component [some-component]
+(r/render-component [app]
                     (.-body js/document))
