0ceb9da46e5886d240502bdfddb927ef1530f1a4
Alexei Matveev <alexei.matveev@gmail.com>
2016-10-16 22:50:29 +0200

    AM: this is how to add externs

--- project.clj
+++ project.clj
@@ -49,6 +49,7 @@
                 :compiler {:output-to "resources/public/js/compiled/leaves.js"
                            :main leaves.core
                            :optimizations :advanced
+                           :externs ["externs.js"]
                            :pretty-print false}}]}
 
   :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
