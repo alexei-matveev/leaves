0b8e8a713ec3dd7f1cdf6f16e98bae02732838f1
Alexei Matveev <alexei.matveev@gmail.com>
2016-10-17 22:55:53 +0200

    AM: another round

--- leaflet.ext.js
+++ leaflet.ext.js
@@ -20,3 +20,30 @@ L.tileLayer = function () {};
  */
 L.Map = function () {};
 L.Map.prototype.addLayer = function() {}
+L.Map.prototype.on = function() {}
+L.Map.prototype.off = function() {}
+
+/**
+ * @return {L.Fake}
+ */
+L.Map.prototype.getPanes = function() {}
+
+L.Fake = function () {};
+/**
+ * @type {HTMLElement}
+ */
+L.Fake.prototype.overlayPane;
+
+/**
+ * @constructor
+ */
+L.Layer = function () {};
+L.Layer.prototype.addLayer = function() {};
+
+
+L.DomUtil = {};
+
+/**
+ * @return {HTMLElement}
+ */
+L.DomUtil.create = function (tagName, className, container) {};
